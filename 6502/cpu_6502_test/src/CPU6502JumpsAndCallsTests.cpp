13903362c0bb899d9c41357b7e6a00b13dd0a5f8
mpiresgro <mpiresgro@gmail.com>
2021-04-12 11:06:19 +0100

    More JumpsAndCalls tests

--- 6502/cpu_6502_test/src/CPU6502JumpsAndCallsTests.cpp
+++ 6502/cpu_6502_test/src/CPU6502JumpsAndCallsTests.cpp
@@ -24,11 +24,9 @@ static void VerifyNotAffectedFlags(cpu6502::CPU &cpu, cpu6502::CPU &cpuCopy)
     // not affected by JMP, JSR, RTS
     EXPECT_EQ(cpu.flags.C, cpuCopy.flags.C);
     EXPECT_EQ(cpu.flags.I, cpuCopy.flags.I);
-    EXPECT_EQ(cpu.flags.Z, cpuCopy.flags.Z);
     EXPECT_EQ(cpu.flags.D, cpuCopy.flags.D);
     EXPECT_EQ(cpu.flags.B, cpuCopy.flags.B);
     EXPECT_EQ(cpu.flags.V, cpuCopy.flags.V);
-    EXPECT_EQ(cpu.flags.N, cpuCopy.flags.N);
 }
 
 TEST_F(CPU6502JumpsAndCallsTests, JumpToSubroutineAndJumpBack)
@@ -50,15 +48,43 @@ TEST_F(CPU6502JumpsAndCallsTests, JumpToSubroutineAndJumpBack)
     // Then:
     EXPECT_EQ(cpu.A, 0x50);
     EXPECT_EQ(CyclesUsed, CyclesExpected);
+    EXPECT_EQ(cpu.SP, cpuCopy.SP);
     VerifyNotAffectedFlags(cpu, cpuCopy);
 }
 
 TEST_F(CPU6502JumpsAndCallsTests, JSRDoesntAffectProcessorStatus)
 {
-
+    // Given:
+    cpu.Reset(mem, 0xFF00);
+    mem[0xFF00] = CPU::INS_JSR;
+    mem[0xFF01] = 0x44;
+    mem[0xFF02] = 0x20;
+    s32 CyclesExpected = 6;
+    CPU cpuCopy = cpu;
+    // When:
+    s32 CyclesUsed = cpu.Execute(CyclesExpected, mem);
+    // Then:
+    EXPECT_EQ(cpu.PS, cpuCopy.PS);
+    EXPECT_NE(cpu.SP, cpuCopy.SP);
+    EXPECT_EQ(CyclesUsed, CyclesExpected);
 }
 
-
+TEST_F(CPU6502JumpsAndCallsTests, RTSDoesntAffectProcessorStatus)
+{
+    // Given:
+    cpu.Reset(mem, 0xFF00);
+    mem[0xFF00] = CPU::INS_JSR;
+    mem[0xFF01] = 0x44;
+    mem[0xFF02] = 0x20;
+    mem[0x2044] = CPU::INS_RTS;
+    s32 CyclesExpected = 6 + 6;
+    CPU cpuCopy = cpu;
+    // When:
+    s32 CyclesUsed = cpu.Execute(CyclesExpected, mem);
+    // Then:
+    EXPECT_EQ(cpu.PS, cpuCopy.PS);
+    EXPECT_EQ(CyclesUsed, CyclesExpected);
+}
 
 TEST_F(CPU6502JumpsAndCallsTests, JumpToLocationAbsolute)
 {
@@ -76,12 +102,27 @@ TEST_F(CPU6502JumpsAndCallsTests, JumpToLocationAbsolute)
     // Then:
     EXPECT_EQ(cpu.A, 0x50);
     EXPECT_EQ(CyclesUsed, CyclesExpected);
-    VerifyNotAffectedFlags(cpu, cpuCopy);
-
+    EXPECT_EQ(cpu.SP, cpuCopy.SP);
+    EXPECT_EQ(cpu.PS, cpuCopy.PS);
 }
-
-
 
 TEST_F(CPU6502JumpsAndCallsTests, JumpToLocationAIndirect)
 {
+    mem[0xFFFC] = CPU::INS_JMP_IND;
+    mem[0xFFFD] = 0x44;
+    mem[0xFFFE] = 0x20;
+    mem[0x2044] = 0x30;
+    mem[0x2045] = 0x02;
+    mem[0x0230] = CPU::INS_LDA_IM;
+    mem[0x0231] = 0x50;
+    // INS_JMP_IND + INS_LDA_IM
+    s32 CyclesExpected = 5 + 2;
+    CPU cpuCopy = cpu;
+    // When:
+    s32 CyclesUsed = cpu.Execute(CyclesExpected, mem);
+    // Then:
+    EXPECT_EQ(cpu.A, 0x50);
+    EXPECT_EQ(CyclesUsed, CyclesExpected);
+    EXPECT_EQ(cpu.SP, cpuCopy.SP);
+    EXPECT_EQ(cpu.PS, cpuCopy.PS);
 }
